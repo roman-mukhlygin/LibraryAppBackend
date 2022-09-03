381015552740d418c3a0307f17078af7a0f882bd
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:20:28 +0300

    Add Book Dto

---
+++ LibraryAppBackend/Dto/BookDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Dto
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        
+    }
+}
