0bf39993d6df2c2ad1e2f7fc733d3d9fd1c5b79b
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:23:09 +0300

    Add Author repository

---
+++ LibraryAppBackend/Infrastructure/Data/AuthorModel/AuthorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Library.Domain;
+
+namespace Library.Infrastructure.Data.AuthorModel
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly LibraryContext _dbContext;
+        public AuthorRepository( LibraryContext dbContext )
+        {
+            _dbContext = dbContext;
+        }
+        public Author GetByName( string name )
+        {
+            return _dbContext.Author.Where( x => x.Name == name ).Single();
+        }
+    }
+}
---
+++ LibraryAppBackend/Infrastructure/Data/AuthorModel/IAuthorRepository.cs
@@ -0,0 +1,9 @@
+﻿using Library.Domain;
+
+namespace Library.Infrastructure.Data.AuthorModel
+{
+    public interface IAuthorRepository
+    {
+        Author GetByName( string name );
+    }
+}
