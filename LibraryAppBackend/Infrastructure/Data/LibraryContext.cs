7494a9ffd7368da6315d9f8300173f1843a24904
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:22:58 +0300

    Add db context

---
+++ LibraryAppBackend/Infrastructure/Data/LibraryContext.cs
@@ -0,0 +1,34 @@
+﻿using Library.Domain;
+using Library.Infrastructure.Data.AuthorModel.EntityConfiguration;
+using Library.Infrastructure.Data.BookModel.EntityConfiguration;
+using Library.Infrastructure.Data.GenreModel.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext( DbContextOptions<LibraryContext> options ) : base( options )
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public async Task<bool> SaveEntitiesAsync()
+        {
+            await SaveChangesAsync();
+            return true;
+        }
+
+        protected override void OnModelCreating( ModelBuilder builder )
+        {
+            base.OnModelCreating( builder );
+            builder.ApplyConfiguration( new BookMap() );
+            builder.ApplyConfiguration( new GenreMap() );
+            builder.ApplyConfiguration( new AuthorMap() );
+        }
+    }
+}
