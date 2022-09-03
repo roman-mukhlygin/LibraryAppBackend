9affc8cf2cdc55f6222a33abd9602b9c77dd4d8e
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:20:38 +0300

    Add Book Dto extension

---
+++ LibraryAppBackend/Dto/BookDtoExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Library.Domain;
+
+namespace Library.Dto
+{
+    public static class BookDtoExtensions
+    {
+        public static Book ConvertToBook( this BookDto bookDto, int genreId, int authorId )
+        {
+            return new Book
+            {
+                Id = bookDto.Id,
+                Name = bookDto.Name,
+                Year = bookDto.Year,
+                GenreId = genreId,
+                AuthorId = authorId
+            };
+        }
+
+        public static BookDto ConvertToBookDto( this Book book, string genre, string author )
+        {
+            return new BookDto
+            {
+                Id = book.Id,
+                Name = book.Name,
+                Year = book.Year,
+                Genre = genre,
+                Author = author
+            };
+        }
+    }
+}
