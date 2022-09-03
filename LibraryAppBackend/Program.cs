6bdf0afba0491c6d309cb82a1aa08c50f578d0e7
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:33:24 +0300

    Register db context and inject dependencies

--- LibraryAppBackend/Program.cs
+++ LibraryAppBackend/Program.cs
@@ -1,8 +1,34 @@
+using LibraryAppBackend.Infrastructure.Data;
+using LibraryAppBackend.Infrastructure.Data.AuthorModel;
+using LibraryAppBackend.Infrastructure.Data.BookModel;
+using LibraryAppBackend.Infrastructure.Data.GenreModel;
+using LibraryAppBackend.Infrastructure.UoW;
+using LibraryAppBackend.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<LibraryContext>( c =>
+{
+    try
+    {
+        string connectionString = builder.Configuration.GetValue<string>( "DefaultConnection" );
+        c.UseSqlServer( connectionString );
+    }
+    catch ( Exception ex )
+    {
+        var message = ex.Message;
+    }
+} );
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<IBookService, BookService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
