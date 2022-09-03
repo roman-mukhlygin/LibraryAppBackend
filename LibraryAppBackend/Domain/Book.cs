acc5d61757f74cf2d6d28c4fbe7343f1896d1084
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:19:52 +0300

    Add Book domain

---
+++ LibraryAppBackend/Domain/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public int GenreId { get; set; }
+        public int AuthorId { get; set; }
+
+        public Genre Genre { get; set; }
+        public Author Author { get; set; }
+    }
+}
