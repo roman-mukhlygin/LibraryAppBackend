575c6c9e299c41c65ec902b2a18ad56bf53ba46d
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:24:01 +0300

    Add Genre entity configuration

---
+++ LibraryAppBackend/Infrastructure/Data/GenreModel/EntityConfiguration/GenreMap.cs
@@ -0,0 +1,16 @@
+﻿using Library.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Infrastructure.Data.GenreModel.EntityConfiguration
+{
+    public class GenreMap : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure( EntityTypeBuilder<Genre> builder )
+        {
+            builder.HasKey( x => x.Id );
+            builder.Property( x => x.Id ).ValueGeneratedOnAdd();
+            builder.Property( x => x.Name );
+        }
+    }
+}
