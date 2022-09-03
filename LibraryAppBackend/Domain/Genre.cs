0cae01e737a0c24ec36c93c5d2c72dbc28264cf4
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:19:47 +0300

    Add Genre Domain

---
+++ LibraryAppBackend/Domain/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Domain
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Book> Books { get; set; }
+    }
+}
