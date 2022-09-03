8f2837ca9c84b3387ab4cb49088ad3b14c9b65e7
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:23:19 +0300

    Add Author entity configuration

---
+++ LibraryAppBackend/Infrastructure/Data/AuthorModel/EntityConfiguration/AuthorMap.cs
@@ -0,0 +1,16 @@
+﻿using Library.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Infrastructure.Data.AuthorModel.EntityConfiguration
+{
+    public class AuthorMap : IEntityTypeConfiguration<Author>
+    {
+        public void Configure( EntityTypeBuilder<Author> builder )
+        {
+            builder.HasKey( x => x.Id );
+            builder.Property( x => x.Id ).ValueGeneratedOnAdd();
+            builder.Property( x => x.Name );
+        }
+    }
+}
