3c40d2e509a1d456ac21002124d16c183e8622d2
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-03 15:23:43 +0300

    Add Book entity configuration

---
+++ LibraryAppBackend/Infrastructure/Data/BookModel/EntityConfiguration/BookMap.cs
@@ -0,0 +1,22 @@
+﻿using Library.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Infrastructure.Data.BookModel.EntityConfiguration
+{
+    public class BookMap : IEntityTypeConfiguration<Book>
+    {
+        public void Configure( EntityTypeBuilder<Book> builder )
+        {
+            builder.HasKey( x => x.Id );
+            builder.Property( x => x.Id ).ValueGeneratedOnAdd();
+            builder.Property( x => x.Name );
+            builder.Property( x => x.Year );
+            builder.Property( x => x.GenreId );
+            builder.Property( x => x.AuthorId );
+
+            builder.HasOne( x => x.Genre ).WithMany( y => y.Books ).HasForeignKey( x => x.GenreId ).OnDelete( DeleteBehavior.Cascade );
+            builder.HasOne( x => x.Author ).WithMany( y => y.Books ).HasForeignKey( x => x.AuthorId ).OnDelete( DeleteBehavior.Cascade );
+        }
+    }
+}
