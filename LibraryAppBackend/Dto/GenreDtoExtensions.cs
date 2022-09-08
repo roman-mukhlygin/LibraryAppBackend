171a35fe823b3f632dd8fca7c4d56f2296c882ac
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-08 22:59:33 +0300

    Add Genre dto

---
+++ LibraryAppBackend/Dto/GenreDtoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using LibraryAppBackend.Domain;
+
+namespace LibraryAppBackend.Dto
+{
+    public static class GenreDtoExtensions
+    {
+        public static GenreDto ConvertToGenreDto( this Genre genre )
+        {
+            return new GenreDto
+            {
+                Id = genre.Id,
+                Name = genre.Name,
+            };
+        }
+    }
+}
