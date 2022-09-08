6e2c5a8c88569628308b64adc4c2877c1bcacfdf
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-08 23:00:07 +0300

    Add Author controller

---
+++ LibraryAppBackend/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using LibraryAppBackend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAppBackend.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+        public AuthorController( IAuthorService authorService )
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        [Route( "list" )]
+        public IActionResult GetAuthorList()
+        {
+            try
+            {
+                return Ok( _authorService.GetAuthorList() );
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+    }
+}
