8537b2a1615bd9a25882af695abf6fa2427d8e2a
Roman Mukhlygin <sonicoorn@gmail.com>
2022-09-08 23:00:15 +0300

    Add Genre controller

---
+++ LibraryAppBackend/Controllers/GenreController.cs
@@ -0,0 +1,31 @@
+﻿using LibraryAppBackend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAppBackend.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly IGenreService _genreService;
+        public GenreController( IGenreService genreService )    
+        {
+            _genreService = genreService;
+        }
+
+        [HttpGet]
+        [Route( "list" )]
+        public IActionResult GetGenreList()
+        {
+            try
+            {
+                return Ok( _genreService.GetGenreList() );
+            }
+            catch ( Exception ex )
+            {
+                return BadRequest( ex.Message );
+            }
+        }
+    }
+}
